a1c9e8b8e1a4e4793c7b0331475e4b74857e4947
Iohan Sardinha <ioxsardinha@gmail.com>
2020-11-02 22:12:41 +0000

    Full arguments

--- main.h
+++ main.h
@@ -10,6 +10,10 @@
 
 extern bool debug;
 
+extern int send_time_out; 
+extern int read_time_out; 
+extern int send_tries; 
+
 int validateArgs(int argc, char const *argv[], int* role, int* port, char* path);
 
 void printUsage();
