2af899b57155ad920a6d219429fad7dc33061a4e
Iohan Sardinha <ioxsardinha@gmail.com>
2020-11-03 13:24:29 +0000

    transfering data

--- ll.c
+++ ll.c
@@ -25,7 +25,7 @@ int llopen(int portN, int role_)
 	printf("Connecting to %s...\n", port);
 
 	(void) signal(SIGALRM, handle_alarm);
-	alarm();
+	alarm(read_time_out);
 
 	fd = open(port, O_RDWR | O_NOCTTY);
 	if (fd <0) {
